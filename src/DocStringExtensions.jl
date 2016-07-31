9636dde2c90875438c88fd41d6f2d55ba5271a71
Michael Hatherly <michaelhatherly@gmail.com>
2016-07-31 14:10:23 +0200

    Bootstrap at toplevel to get better coverage

--- src/DocStringExtensions.jl
+++ src/DocStringExtensions.jl
@@ -33,8 +33,24 @@ include("utilities.jl")
 include("abbreviations.jl")
 
 
+#
 # Bootstrap abbreviations.
-
-bootstrap(DocStringExtensions)
+#
+# Within the package itself we would like to be able to use the abbreviations that have been
+# implemented. To do this we need to delay evaluation of the interpolated abbreviations
+# until they have all been defined. We use `Symbol`s in place of the actual constants, such
+# as `methodlist` which is written as `:methodlist` instead.
+#
+# The docstring for the module itself, defined at the start of the file, does not need to
+# use `Symbol`s since with the way `@doc` works the module docstring gets inserted at the
+# end of the module definition and so has all the definitions already defined.
+#
+let λ = s -> isa(s, Symbol) ? getfield(DocStringExtensions, s) : s
+    for (binding, multidoc) in Docs.meta(DocStringExtensions)
+        for (typesig, docstr) in multidoc.docs
+            docstr.text = Core.svec(map(λ, docstr.text)...)
+        end
+    end
+end
 
 end # module
--- src/utilities.jl
+++ src/utilities.jl
@@ -39,31 +39,3 @@ function parsedocs(mod::Module)
     end
 end
 
-
-#
-# Bootstrap
-#
-
-"""
-Within the package itself we would like to be able to use the abbreviations that have been
-implemented. To do this we need to delay evaluation of the interpolated abbreviations until
-they have all been defined. We use `Symbol`s in place of the actual constants, such as
-`methodlist` which is written as `:methodlist` instead.
-
-$(:methodlist)
-
-!!! note
-
-    The docstring for the module itself, defined at the start of the file, does not need to
-    use `Symbol`s since with the way `@doc` works the module docstring gets inserted at the
-    end of the module definition and so has all the definitions already defined.
-"""
-function bootstrap(mod::Module)
-    λ = s -> isa(s, Symbol) ? getfield(mod, s) : s
-    for (binding, multidoc) in Docs.meta(mod)
-        for (typesig, docstr) in multidoc.docs
-            docstr.text = Core.svec(map(λ, docstr.text)...)
-        end
-    end
-end
-
