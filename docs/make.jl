62bde98de8b760908e3734bbdf19838f20a69783
Michael Hatherly <michaelhatherly@gmail.com>
2016-08-16 14:34:08 +0200

    Test out HTML rendering for docs

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,17 @@ VERSION < v"0.5.0-dev" && exit()
 using Documenter, DocStringExtensions
 
 makedocs(
+    sitename = "DocStringExtensions.jl",
     modules = [DocStringExtensions],
+    format = Documenter.Formats.HTML,
     clean = false,
+    pages = Any["Home" => "index.md"],
 )
 
 deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material"),
+    target = "build",
+    deps = nothing,
+    make = nothing,
     repo = "github.com/JuliaDocs/DocStringExtensions.jl.git",
 )
 
