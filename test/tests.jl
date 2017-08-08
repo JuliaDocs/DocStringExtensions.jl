a1a269c20a7b3ee2511ac79e4dc45331c7bd3243
Twan Koolen <koolen.twan@gmail.com>
2017-08-08 19:23:04 -0400

    Add tests.

--- test/tests.jl
+++ test/tests.jl
@@ -17,6 +17,7 @@ const A{T} = Union{Vector{T}, Matrix{T}}
 
 h_1(x::A) = x
 h_2(x::A{Int}) = x
+h_3(x::A{T}) where {T} = x
 
 mutable struct T
     a
@@ -297,6 +298,9 @@ end
             @test length(DSE.getmethods(M.f, Union{})) == 1
             @test length(DSE.getmethods(M.f, Tuple{})) == 0
             @test length(DSE.getmethods(M.f, Union{Tuple{}, Tuple{Any}})) == 1
+            @test length(DSE.getmethods(M.h_3, Tuple{M.A{Int}})) == 1
+            @test length(DSE.getmethods(M.h_3, Tuple{Vector{Int}})) == 1
+            @test length(DSE.getmethods(M.h_3, Tuple{Array{Int, 3}})) == 0
         end
         @testset "methodgroups" begin
             @test length(DSE.methodgroups(M.f, Tuple{Any}, M)) == 1
@@ -305,11 +309,13 @@ end
             @test length(DSE.methodgroups(M.h_1, Tuple{M.A}, M)[1]) == 1
             @test length(DSE.methodgroups(M.h_2, Tuple{M.A{Int}}, M)) == 1
             @test length(DSE.methodgroups(M.h_2, Tuple{M.A{Int}}, M)[1]) == 1
+            @test length(DSE.methodgroups(M.h_3, Tuple{M.A}, M)[1]) == 1
         end
         @testset "alltypesigs" begin
             @test DSE.alltypesigs(Union{}) == Any[]
             @test DSE.alltypesigs(Union{Tuple{}}) == Any[Tuple{}]
             @test DSE.alltypesigs(Tuple{}) == Any[Tuple{}]
+            @test DSE.alltypesigs(Type{T} where {T}) == Any[Type{T} where T]
         end
         @testset "groupby" begin
             let groups = DSE.groupby(Int, Vector{Int}, collect(1:10)) do each
