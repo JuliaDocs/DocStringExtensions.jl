2c1bdaaeb167212c210f18538b2499d775bc07b6
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-03-20 09:05:44 +0100

    contains(haystack, needle) -> occursin(needle, haystack)

--- src/utilities.jl
+++ src/utilities.jl
@@ -246,7 +246,7 @@ function keywords(func, m::Method)
         # Fetching method keywords stolen from base/replutil.jl:572-576 (commit 3b45cdc9aab0):
         local kwargs = Base.kwarg_decl(m, typeof(table.kwsorter))
         if isa(kwargs, Vector) && length(kwargs) > 0
-            filter!(arg -> !contains(string(arg), "#"), kwargs)
+            filter!(arg -> !occursin("#", string(arg)), kwargs)
             # Keywords *may* not be sorted correctly. We move the vararg one to the end.
             local index = findfirst(arg -> endswith(string(arg), "..."), kwargs)
             if index != nothing && index > 0 # TODO: use Compat.findfirst later on
