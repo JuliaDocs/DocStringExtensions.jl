b68b675700929fb460dd8b2cffb874c1e190e57c
Tamas K. Papp <tkpapp@gmail.com>
2018-08-27 13:53:52 +0200

    Add the FUNCTIONNAME abbreviation.
    Fixes #59.

--- src/abbreviations.jl
+++ src/abbreviations.jl
@@ -303,6 +303,46 @@ function format(::MethodSignatures, buf, doc)
     end
 end
 
+#
+# `FunctionName`
+#
+
+"""
+The singleton type for [`FUNCTIONNAME`](@ref) abbreviations.
+
+$(:FIELDS)
+"""
+struct FunctionName <: Abbreviation end
+
+"""
+An [`Abbreviation`](@ref) for including the function name matching the method of
+the docstring.
+
+# Usage
+
+This is mostly useful for not repeating the function name in docstrings where
+the user wants to retain full control of the argument list, or the latter does
+not exist (eg generic functions).
+
+Note that the generated docstring snippet is not quoted, use indentation or
+explicit quoting.
+
+# Example
+
+```julia
+\"""
+    \$(FUNCTIONNAME)(d, θ)
+
+Calculate the logdensity `d` at `θ`.
+
+Users should define their own methods for `$(FUNCTIONNAME)`.
+\"""
+function logdensity end
+```
+"""
+const FUNCTIONNAME = FunctionName()
+
+format(::FunctionName, buf, doc) = print(buf, doc.data[:binding].var)
 
 #
 # `TypeSignature`
