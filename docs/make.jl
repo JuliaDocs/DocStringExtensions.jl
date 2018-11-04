1668567d66be799e5f94a577bf6d8bc564ca8c7d
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-11-04 02:22:50 +0100

    Add README and LICENSE (#68)
    * Add README and LICENSE to make it possible to splice in
    the package README.md and LICENSE.md in docstrings.
    
    * Updates for Documenter v0.20.

--- docs/make.jl
+++ docs/make.jl
@@ -3,16 +3,10 @@ using Documenter, DocStringExtensions
 makedocs(
     sitename = "DocStringExtensions.jl",
     modules = [DocStringExtensions],
-    format = :html,
     clean = false,
     pages = Any["Home" => "index.md"],
 )
 
 deploydocs(
-    target = "build",
-    deps = nothing,
-    make = nothing,
     repo = "github.com/JuliaDocs/DocStringExtensions.jl.git",
-    julia = "1.0",
 )
-
