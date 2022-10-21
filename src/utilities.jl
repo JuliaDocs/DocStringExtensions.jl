c30c4c9be0a8518d0b04a585996db30df5fafd3f
KristofferC <kristoffer.carlsson@juliacomputing.com>
2022-10-21 13:23:13 +0200

    fix kw handling on master

--- src/utilities.jl
+++ src/utilities.jl
@@ -419,7 +419,7 @@ function keywords(func, m::Method)
     # table is a MethodTable object. For some reason, the :kwsorter field is not always
     # defined. An undefined kwsorter seems to imply that there are no methods in the
     # MethodTable with keyword arguments.
-    if isdefined(table, :kwsorter)
+    if  !hasfield(Core.MethodTable, :kwsorter) || isdefined(table, :kwsorter)
         # Fetching method keywords stolen from base/replutil.jl:572-576 (commit 3b45cdc9aab0):
         kwargs = VERSION < v"1.4.0-DEV.215" ? Base.kwarg_decl(m, typeof(table.kwsorter)) : Base.kwarg_decl(m)
         if isa(kwargs, Vector) && length(kwargs) > 0
@@ -463,7 +463,7 @@ function arguments(m::Method)
         local args = map(argnames[1:nargs(m)]) do arg
             arg === Symbol("#unused#") ? "_" : arg
         end
-        return filter(arg -> arg !== Symbol("#self#"), args)
+        return filter(arg -> arg !== Symbol("#self#") && arg !== Symbol("#ctor-self#"), args)
     end
     return Symbol[]
 end
